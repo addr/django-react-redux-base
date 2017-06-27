b8aedf25591cea9286ddb49b890f1896f012da6f
Dillon Johnson <djrepec@uga.edu>
2017-06-27 16:03:07 -0400

    Put iteration in update

--- src/feedback/views.py
+++ src/feedback/views.py
@@ -103,12 +103,16 @@ class FeedbackUpdate(GenericAPIView):
         serializer = FeedbackUpdateSerializer(data=request.data)
         if serializer.is_valid():
             feedback = Feedback.objects.filter(feedback_id=serializer.data['search_feedback_id'])
-            print(feedback[0])
-            print(serializer.data['initial_comment'])
-            print(serializer.data['student_rating'])
-            feedback[0].initial_comment = serializer.data['initial_comment']
-            feedback[0].student_rating = serializer.data['student_rating']
-            feedback[0].save()
+            # print(feedback[0])
+            # print(serializer.data['initial_comment'])
+            # print(serializer.data['student_rating'])
+            # feedback[0].initial_comment = serializer.data['initial_comment']
+            # feedback[0].student_rating = serializer.data['student_rating']
+            # feedback[0].save()
+            for f in feedback:
+                f.initial_comment = serializer.data['initial_comment']
+                f.student_rating = serializer.data['student_rating']
+                f.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
 
